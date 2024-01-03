78a2a10eb02193f30090223ba7cb454763e81820
Blake Wollston <57561522+InterPixelCoding@users.noreply.github.com>
2024-01-03 18:34:21 +0000

    Added New Website + Timeline

--- script.js
+++
@@ -1,7 +0,0 @@
-const menubtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('nav');
-
-menubtn.addEventListener("click", function () {
-    menu.classList.toggle('open')
-    menubtn.classList.toggle('close')
-})
